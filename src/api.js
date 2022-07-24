02622f27f1680b4f1815294261fde3d8b8733b88
miguelcastiyo <44275086+miguelcastiyo@users.noreply.github.com>
2022-07-23 20:12:25 -0700

    React weather app!

---
+++ src/api.js
@@ -0,0 +1,14 @@
+
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'be71e85ba8mshe0b54d7ecd4f1bap1e8d8bjsn85c65562e47c',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"; 
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "f71f255475d03dc616015b498dc808d6";
